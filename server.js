9c0efd27a55e50c4a247f7ab01b25d27d40ced95
toshan kanwar <toshankanwar03@gmail.com>
2026-03-14 22:19:02 +0530

    fix

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 mongoose.connect(
-"mongodb+srv://toshankanwar03_db_user:uSCqOralOPe3snHs@fiidii-databse.hgvxszn.mongodb.net/market"
+"mongodb+srv://toshankanwar03_db_user:uSCqOralOPe3snHs@fiidii-databse.hgvxszn.mongodb.net:27017/market"
 )
 .then(()=>console.log("MongoDB connected"))
 .catch(err=>console.log(err));
